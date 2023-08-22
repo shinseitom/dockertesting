1fbdcda094f0e2226aed7bff222ae9ed1708f686
shinseitom <53927798+shinseitom@users.noreply.github.com>
2023-08-21 22:11:43 -0500

    Add files via upload

--- test.sh
+++ test.sh
@@ -4,21 +4,21 @@
 set -x
 
 # download the env archipelago version
-curl $1+$2+".tar.gz"
+#curl $1+$2+".tar.gz"
 
 # decompress
-tar -xf "Archipelago-"+$2+".tar.gz"
+#tar -xf "Archipelago-"+$2+".tar.gz"
 
 # copy base roms to the archipelago folder
-cp -a /baseroms/. "/Archipelago-"+$2
+#cp -a /baseroms/. "/Archipelago-"+$2
 
 # move to new folder
-cd "Archipelago-"+$2
+#cd "Archipelago-"+$2
 
 # install setup requirements
-echo "\n" | python3 -u setup.py
+#echo "\n" | python3 -u setup.py
 
 # start webhost (and probably install more requirements)
-echo "\n" | python3 -u WebHost.py
+#echo "\n" | python3 -u WebHost.py
 
 echo "you shouldn't see this unless stopping manually"
